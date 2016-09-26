3ac49a58f268aa49d81642cb7698159b939e3ee9
lrengunst <noreply>
2016-09-26 09:36:13 +0000

    Add component converter

---
+++ ui/converter.reel/converter.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/converter.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Converter
+ * @extends Component
+ */
+exports.Converter = Component.specialize(/** @lends Converter# */ {
+    constructor: {
+        value: function Converter() {
+            this.super();
+        }
+    }
+});
